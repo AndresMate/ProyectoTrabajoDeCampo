f9d0648441588e8ca12d650ddf1e4b5b6ed27d48
Andres Felipe Mateus Riaño <105401959+AndresMate@users.noreply.github.com>
2025-09-30 15:16:44 -0500

    inicializar bacend

---
+++ backend_TC/src/main/java/co/edu/uptc/backend_tc/BackendTcApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.backend_tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTcApplication.class, args);
+    }
+
+}
---
+++ backend_TC/src/test/java/co/edu/uptc/backend_tc/BackendTcApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.backend_tc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
